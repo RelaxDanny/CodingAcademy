8d45818fdb49cee6c03059d5e6606f7ed04abf93
rxdanny <44015667+RelaxDanny@users.noreply.github.com>
2020-02-11 09:11:26 +0900

    Update CrawlingPractice.py

--- CrawlingPractice.py
+++ CrawlingPractice.py
@@ -1,30 +1,15 @@
-# #if bs4 is not working, use selenium
+#if bs4 is not working, use selenium
 
-# from bs4 import BeautifulSoup
-# from urllib.request import urlopen
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
 
-# #response = urlopen('https://en.wikipedia.org/wiki/Main_Page')
-
-# with urlopen('https://en.wikipedia.org/wiki/Main_Page') as response:
-#     soup = BeautifulSoup(response, 'html.parser')
-#     for anchor in soup.select("span.ah_k"):
-#         print(anchor)
-from selenium import webdriver
-import bs4
-driver = webdriver.Chrome(r"C:\Users\김영호\Desktop\github\chromedriver.exe")
-driver.implicitly_wait(3) #wait for 3 to wait for the chrome to be opend
-
-# driver.get("https://nid.naver.com/nidlogin.login")
-
-# driver.find_element_by_name('id').send_keys('dannyinsuny')
-# driver.find_element_by_name('pw').send_keys('!Smlal153') 
-# #But this will lead to the "I'm not robot" page.
-# driver.find_element_by_xpath('//*[@id="frmNIDLogin"]/fieldset/input').click()
-
-driver.get('https://order.pay.naver.com/home')
-html = driver.page_source
-soup = bs4.BeautifulSoup(html, 'html.parser')
-notices = soup.findall("a", {"class":"goods"})
-
-for n in notices:
-    print(n.text.strip())
+#response = urlopen('https://en.wikipedia.org/wiki/Main_Page')
+i = 1
+f = open("test.txt", 'w')
+with urlopen('https://en.wikipedia.org/wiki/Main_Page') as response:
+    soup = BeautifulSoup(response, 'html.parser')
+    for anchor in soup.select("a"):
+        data = str(i) + ". " + anchor.get('href', '/') + "\n" 
+        i = i + 1
+        f.write(data)
+f.close()
