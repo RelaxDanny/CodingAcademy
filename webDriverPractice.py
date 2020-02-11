ed53f34348be0dafdb0730d8ed59aeb34e74ced1
rxdanny <44015667+RelaxDanny@users.noreply.github.com>
2020-02-11 09:11:59 +0900

    Create webDriverPractice.py

---
+++ webDriverPractice.py
@@ -0,0 +1,20 @@
+
+from selenium import webdriver
+import bs4
+driver = webdriver.Chrome(r"C:\Users\김영호\Desktop\github\chromedriver.exe")
+driver.implicitly_wait(3) #wait for 3 to wait for the chrome to be opend
+
+driver.get("https://nid.naver.com/nidlogin.login")
+
+driver.find_element_by_name('id').send_keys('dannyinsuny')
+driver.find_element_by_name('pw').send_keys('!Smlal153') 
+#But this will lead to the "I'm not robot" page.
+driver.find_element_by_xpath('//*[@id="frmNIDLogin"]/fieldset/input').click()
+
+driver.get('https://order.pay.naver.com/home')
+html = driver.page_source
+soup = bs4.BeautifulSoup(html, 'html.parser')
+notices = soup.findall("a", {"class":"goods"})
+
+for n in notices:
+    print(n.text.strip())
