6347b496066259eedd976b88d0edb28315d8e0da
rxdanny <44015667+RelaxDanny@users.noreply.github.com>
2020-02-11 15:26:10 +0900

    Update google.py

--- google.py
+++ google.py
@@ -2,6 +2,6 @@ from google_images_download import google_images_download   #importing the libra
 
 response = google_images_download.googleimagesdownload()   #class instantiation
 
-arguments = {"keywords":"강다니엘","limit":20,"print_urls":True, "format": "jpg"}   #creating list of arguments
+arguments = {"keywords":"Tesla","limit":100,"print_urls":True, "format": "jpg"}   #creating list of arguments
 paths = response.download(arguments)   #passing the arguments to the function
 print(paths)   #printing absolute paths of the downloaded images
