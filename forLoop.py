27e8252b5c42eed7335961f7041c3dbed10e5cab
rxdanny <44015667+RelaxDanny@users.noreply.github.com>
2020-02-14 00:39:31 +0900

    Create forLoop.py

---
+++ forLoop.py
@@ -0,0 +1,12 @@
+example_list = [1,2,3,9,8,7,10,111,2455]
+
+for eachNumber in example_list:
+    print(eachNumber)
+
+print("Continum Program")
+
+count = 0
+
+while count < 3:
+    print('횟수: ', count)
+    count += 1
