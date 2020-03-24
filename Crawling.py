9a60d316cf38fea69224c86eeeafcdb8ea1214f9
rxdanny <44015667+RelaxDanny@users.noreply.github.com>
2020-03-24 17:36:31 +0900

    Create Crawling.py

---
+++ Crawling.py
@@ -0,0 +1,12 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import ssl
+
+context = ssl._create_unverified_context()
+result = urlopen("https://en.wikipedia.org/wiki/Kevin_Bacon", context=context)
+bsObj = BeautifulSoup(result.read(), "html.parser")
+
+for tag in bsObj.findAll("a"):
+    if "href" in tag.attrs:
+        print(tag.attrs["href"])
+
