42d8751515e430e39b28b6a9de15ad2a28213e34
rxdanny <44015667+RelaxDanny@users.noreply.github.com>
2020-04-02 01:05:29 +0900

    Update NaverCrawl.py

--- NaverCrawl.py
+++ NaverCrawl.py
@@ -14,4 +14,19 @@ html = urlopen(url).read()
 soup = BeautifulSoup(html, 'html.parser') #html에 저장된 url을 분석
 img = soup.find_all(class_="_img")
 
+#for loop전에 프린트보여주기
+print(img)
 print(img[0])
+n = 1  # 이걸 안하면 덮어쓰기가 되어버림.
+for i in img:
+    #print(i['data-source'])
+    imgUrl = i['data-source']
+    with urlopen(imgUrl) as f:
+        with open('./img_for_learning/' + plusUrl + str(n) + '.jpg', 'wb') as h:#그냥 w가아니라 wb인 이유는 이미지는 바이너리 파일이기 때문
+            img = f.read()
+            h.write(img)
+    print("다운 완료:", n, "번째",imgUrl)
+    n += 1
+
+print('Download finish')        
+    
