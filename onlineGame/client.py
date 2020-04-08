4c222049e7cbbeac576eefe90c3de5fe7c7cc136
rxdanny <44015667+RelaxDanny@users.noreply.github.com>
2020-04-08 23:58:44 +0900

    Update client.py

--- onlineGame/client.py
+++ onlineGame/client.py
@@ -2,11 +2,10 @@ import pygame
 
 width = 500
 height = 500
-
 win = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Client")
 
-clientNuimber = 0 
+clientNumber = 0
 
 
 class Player():
@@ -16,42 +15,49 @@ class Player():
         self.width = width
         self.height = height
         self.color = color
-        self.vel = 5
-        self.rect = (x, y, width, height)
+        self.rect = (x,y,width,height)
+        self.vel = 3
 
     def draw(self, win):
         pygame.draw.rect(win, self.color, self.rect)
-    
+
     def move(self):
         keys = pygame.key.get_pressed()
+
         if keys[pygame.K_LEFT]:
             self.x -= self.vel
+
         if keys[pygame.K_RIGHT]:
             self.x += self.vel
+
         if keys[pygame.K_UP]:
             self.y -= self.vel
+
         if keys[pygame.K_DOWN]:
             self.y += self.vel
 
         self.rect = (self.x, self.y, self.width, self.height)
 
-def redrawWindow(win, player):
-    win.fill((255, 255, 255)) # white
+
+def redrawWindow(win,player):
+    win.fill((255,255,255))
     player.draw(win)
-    pygame.display.update() # 이걸 사용해야지만 파이게임이 켜진다 -> 업데이트 하는것.
+    pygame.display.update()
 
 
 def main():
-    p1 = Player(50, 50, 100, 100, (0,100,0))
     run = True
+    p = Player(50,50,100,100,(0,255,0))
     clock = pygame.time.Clock()
-    while run:        
+
+    while run:
         clock.tick(60)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 pygame.quit()
-        p1.move()
-        redrawWindow(win,p1)
+
+        p.move()
+        redrawWindow(win, p)
 
 main()
