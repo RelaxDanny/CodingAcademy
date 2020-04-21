e39e79853a0c453422183676e779737fa925681a
Youngho Kim <44015667+rxYoungho@users.noreply.github.com>
2020-04-21 23:10:41 +0900

    Update network_SH.py

--- onlineGame/network_SH.py
+++ onlineGame/network_SH.py
@@ -1,26 +1,30 @@
 import socket
+import pickle
 
 class network():
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server = "10.12.49.214"
+        self.server = "10.12.49.24"
         self.port = 5555
         self.addr = (self.server, self.port) #ip 주소, 포트 --> 튜플
-        self.pos = self.connect() # id = connect를 통한 return 값
+        self.p = self.connect() # id = connect를 통한 return 값
         
-    def getPos(self):
-        return self.pos
+    def getP(self):
+        return self.p
 
     def connect(self):
         try:
             self.client.connect(self.addr) #
-            return self.client.recv(2048).decode() # String 
+            # return pickle.loads(self.client.recv(10000))
+            return self.client.recv(2048).decode()
         except:
             pass
 
     def send(self, data):
         try: 
-            self.client.send(str.encode(data)) # encode 누군가에게 보내기 위한 준비! 포장꾸러미
-            return self.client.recv(2048).decode() # decode 포장꾸러미 풀기!
+            # self.client.send(pickle.dumps(data)) # encode 누군가에게 보내기 위한 준비! 포장꾸러미
+            self.client.send(str.encode(data))
+            return pickle.loads(self.client.recv(2048)) 
         except socket.error as e:
             print(e)
+
