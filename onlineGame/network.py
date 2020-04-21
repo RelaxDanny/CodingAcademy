16eb5bea049c26d36db2291d6512dbd48723fca2
Youngho Kim <44015667+rxYoungho@users.noreply.github.com>
2020-04-21 23:10:39 +0900

    Update network.py

--- onlineGame/network.py
+++ onlineGame/network.py
@@ -1,28 +1,31 @@
-
 import socket
+import pickle
 
 
 class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server = "10.12.49.214"
+        self.server = "10.12.49.24"
         self.port = 5555
         self.addr = (self.server, self.port)
-        self.pos = self.connect()
+        self.p = self.connect()
 
-    def getPos(self):
-        return self.pos
+    def getP(self):
+        return self.p
 
     def connect(self):
         try:
             self.client.connect(self.addr)
-            return self.client.recv(2048).decode()
+            #return pickle.loads(self.client.recv(2048)) pickle을 풀지는 않고 이번엔 디코드 할거임
+            return self.client.recv(2048).decode() #시작하자 받는 정보는 플레이어의 정보이다 0 또는 1 (1p 또는 2p)
+            #그럼 커넥트는 플레이어가 누구인지를 리턴함
         except:
             pass
 
     def send(self, data):
         try:
+            # self.client.send(pickle.dumps(data))
             self.client.send(str.encode(data))
-            return self.client.recv(2048).decode()
+            return pickle.loads(self.client.recv(2048))
         except socket.error as e:
             print(e)
