46c0f5e3934d4c4cf8f2b9a0a5b545ac459d658b
Youngho Kim <44015667+rxYoungho@users.noreply.github.com>
2020-04-21 23:10:45 +0900

    Update client_SH.py

--- onlineGame/client_SH.py
+++ onlineGame/client_SH.py
@@ -1,76 +1,44 @@
 import pygame
 from network_SH import network
+from player import Player
 
-width = 500
-height = 500
+pygame.font.init()
+
+width = 700
+height = 700
 
 win = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Manhattan")
 
-clientNumber = 0
-
-class Player(object):
-    def __init__(self, x, y, width, height, color):
+class Button:
+    def __init__(self, text, x, y, color):
+        self.text = text
         self.x = x
         self.y = y
-        self.width = width
-        self.height = height
         self.color = color
-        self.vel = 5
-        self.rect = (x, y, width, height)
+        self.width = 150
+        self.height = 100
 
     def draw(self, win):
-        pygame.draw.rect(win, self.color, self.rect)
-
-    def move(self):
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT]:
-            self.x -= self.vel
-        if keys[pygame.K_RIGHT]:
-            self.x += self.vel
-        if keys[pygame.K_UP]:
-            self.y -= self.vel
-        if keys[pygame.K_DOWN]:
-            self.y += self.vel
-
-        self.update()
-
-    def update(self):
-        self.rect = (self.x, self.y, self.width, self.height)
-
-def read_pos(str):
-    str = str.split(",") # ,형태로스트링을 나눠라 24, 30
-    return int(str[0]), int(str[1])
-
-def make_pos(tup):
-    return str(tup[0]) + "," + str(tup[1]) # -. 24, 30 ->  "24, 30"
+        pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height))
+        font = pygame.font.SysFont("comicsans", 40)
+        text = font.render(self.text, 1, (255,255,255))
+        win.blit(text, (self.x + round(self.width/2 - round(text.get_width()/2)), self.y + round(self.height/2 - round(text.get_height()/2))))
+    
+    def click(self, pos):
+        x1 = pos[0] #mouse's x
+        y1 = pos[1] #mouse's y
+        if self.x <= x1 <= self.x + self.width and self.y <= y1 <= self.y + self.height:
+            return True
+        else:
+            return False
 
 def redrawWindow(win, player, player2):
-    win.fill((255, 255, 255))
-    player.draw(win)
-    player2.draw(win)
-    pygame.display.update() #업데이트 
+    win.fill((128, 128, 128))
+    pass 
     
+btns = [Button("Rock", 50, 500, (0,0,0)), Button("Scissors", 250, 500, (255,0,0)), Button("Paper", 450, 500, (0,255,0))]
 def main():
-    n = network()
-    startPos = read_pos(n.getPos()) #getPos의 리턴값은 connect()의 리턴값과 같다 -> 받은 데이터 -> 내가 받은 포지션 -> (50, 50) =(x,y) 
-    p1 = Player(startPos[0], startPos[1], 100, 100, (10, 100, 100))
-    p2 = Player(0, 0, 100, 100, (200, 0, 0))
-    run = True
-    clock = pygame.time.Clock()
-    while run:
-        clock.tick(60)
-        p2Pos = read_pos(n.send(make_pos((p1.x, p1.y))))
-        p2.x = p2Pos[0]
-        p2.y = p2Pos[1]
-        p2.update()
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-                pygame.quit()
-
-        p1.move()
-        redrawWindow(win, p1, p2)
+    pass
 
 main()
