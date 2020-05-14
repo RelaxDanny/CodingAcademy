7b39d0f8eef6874d6fe7875555334e93de22e266
Youngho Kim <44015667+rxYoungho@users.noreply.github.com>
2020-05-14 23:02:51 +0900

    Update CSE352_Laplace.py

--- CSE352_Laplace.py
+++ CSE352_Laplace.py
@@ -8,6 +8,7 @@ selected_sentence = []
 k = [0.5, 1.0, 2.0]
 uni_counts = dict()
 bi_counts = dict()
+N_data = 0
 
 # open text file
 if sentence_file.mode == "r":
@@ -19,17 +20,19 @@ for i in range(len(sentences)):
 #erases the punctuations and spaces
 each_sentence_list = [''.join(c for c in s if c not in string.punctuation) for s in each_sentence_list]
 each_sentence_list = [s for s in each_sentence_list if s]
-print(each_sentence_list[i].split())
-#Create dictionary for UNIGRAM:
+# print(each_sentence_list[i].split())
+
+#Create dictionary for the UNIGRAM:
 for i in range(len(each_sentence_list)):
     for each_word in each_sentence_list[i].split():
+        N_data += 1
         if each_word in uni_counts:
             uni_counts[each_word] += 1
         else:
             uni_counts[each_word] = 1
+# print(N_data)
 
-
-#Create dictionary for BIGRAM:
+#Create dictionary for the BIGRAM:
 for i in range(len(each_sentence_list)):
     for j in range(1, len(each_sentence_list[i].split())):
         each_word = ((each_sentence_list[i].split())[j-1], (each_sentence_list[i].split())[j])
@@ -49,42 +52,37 @@ for j in range(len(each_sentence_list)):
 selected_sentence = [''.join(c for c in s if c not in string.punctuation) for s in selected_sentence]
 selected_sentence = [s for s in selected_sentence if s]
 
-# Test if the data is clearly set.
-# for key, value in bi_counts.items():
-#     if value == 5 :
-#         print(key)
-
 #10 random sentences for each test
-#Store the 10 selected sentences.
-
 for i in range(10):
     randNum = randint(0,len(selected_sentence))
     
     # Unigram method
     pr_word_unigram = 0
     pr_sentence_unigram = 1
-    N_unigram = len(uni_counts)
+    N_unigram = N_data
+    V_unigram = len(uni_counts)
 
     each_word = ""
     for each in selected_sentence[randNum].split():
         each_word = each
     for each_k in k:
-        pr_word_unigram = (uni_counts[each_word]) / (N_unigram)
+        pr_word_unigram = (uni_counts[each_word] + each_k) / (N_unigram + (V_unigram * each_k))
         pr_sentence_unigram = pr_sentence_unigram * pr_word_unigram
         print("When K = ", each_k,":", selected_sentence[randNum],"UNIGRAM: ",pr_sentence_unigram)
     
     # Bigram method
     pr_word_bigram = 0
     pr_sentence_bigram = 1
-    N_bigram = len(bi_counts)
+    N_bigram = N_data
+    V_bigram = len(bi_counts)
     
     for i in range(1, len(selected_sentence[randNum].split())):
         bi_words = ((selected_sentence[randNum].split())[i-1], (selected_sentence[randNum].split())[i])
-        # print(bi_words)
     for each_k in k:
-        pr_word_bigram = (bi_counts[bi_words] + each_k) / (uni_counts[bi_words[0]] + (each_k * N_bigram))
+        pr_word_bigram = (bi_counts[bi_words] + each_k) / (uni_counts[bi_words[0]] + (V_bigram * each_k))
         pr_sentence_bigram = pr_sentence_bigram * pr_word_bigram
         print("When K = ", each_k,":", selected_sentence[randNum],"BIGRAM: ",pr_sentence_bigram)
+    print("--"*60)
 
         
 
