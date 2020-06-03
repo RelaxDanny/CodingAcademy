d6602e325211b541fa5eaebc9e4727c873e7d577
Youngho Kim <44015667+rxYoungho@users.noreply.github.com>
2020-06-03 19:32:51 +0900

    Update BinaryTree_SH.py

--- Datastructure/BinaryTree_SH.py
+++ Datastructure/BinaryTree_SH.py
@@ -19,6 +19,7 @@ Search:
     2. Inorder 형식또는 Binary Search 의 halving 형식으로 값을 찾는다.
     3. 
 """
+
 class Node:
     def __init__(self, data=None):
         self.data = data
@@ -30,19 +31,48 @@ class BST:
         self.root = None
     def insert(self, data):
         #TO DO
-        pass
+        if self.root is None:
+            self.root = Node(data)
+        else:
+            self._insert(data, self.root)
     def _insert(self, data, cur_node):
         #TO Do
-        pass
+        if data < cur_node.data:
+            if cur_node.left is None:
+                cur_node.left = Node(data)
+            else:
+                self._insert(data, cur_node.left)
+        elif data > cur_node.data:
+            if cur_node.right is None:
+                cur_node.right = Node(data)
+            else:
+                self._insert(data, cur_node.right)
+
     def find(self, data): #True or False
         #To Do
-        
-        pass
+        if self.root:
+            is_found = self._find(data, self.root) #Boolean Type (True Or False)
+            if is_found:
+                return True
+            return False
+        else: # if no nodes in the tree
+            return False
+
     def _find(self, data, cur_node):
         #To do
-        pass
+        if data > cur_node.data and cur_node.right:
+            return self._find(data,cur_node.right)
+        elif data < cur_node.data and cur_node.left:
+            return self._find(data, cur_node.left)
+        ##  결국에 찾을떄 까지
+        if data == cur_node.data:
+            return True
+        
 
 bst = BST()
-bst.insert()
+bst.insert(4)
+bst.insert(3)
+bst.insert(2)
+bst.insert(1)
 
-print(bst.find())
+print(bst.find(5))
