c0d962899f2da4a4b47733fe80392ba924321d12
Youngho Kim <44015667+rxYoungho@users.noreply.github.com>
2020-06-29 21:12:03 +0900

    Update flappy_bird_SH.py

--- FlappyBirdAi/flappy_bird_SH.py
+++ FlappyBirdAi/flappy_bird_SH.py
@@ -161,7 +161,7 @@ class Base:
 
 
 
-def draw_window(win, bird, pipes, base, score):
+def draw_window(win, birds, pipes, base, score):
     win.blit(BG_IMG, (0,0))
     
     for pipe in pipes:
@@ -171,53 +171,92 @@ def draw_window(win, bird, pipes, base, score):
     win.blit(score_label, (WIN_WIDTH - score_label.get_width() - 15, 10))
 
     base.draw(win)
-    bird.draw(win)
+    for bird in birds:
+        bird.draw(win)
     pygame.display.update()
 
-def main():
+def main(genomes, config): 
+    nets = []
+    ge = [] 
+    birds = [] #이제부터 bird는 여러마리가 되기 때문에
+
+    for _, g in genomes:
+        net = neat.nn.FeedForwardNetwork.create(g, config)
+        nets.append(net)
+        birds.append(Bird(230,350))
+        g.fitness = 0
+        ge.append(g)
+
+    win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
     base = Base(730)
     pipes = [Pipe(700)]
-    win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
-
     score = 0
-    bird = Bird(200, 200)
     run = True
     clock = pygame.time.Clock()
     while run:
-        clock.tick(30)
+        clock.tick(100)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+                pygame.quit()
+                quit()
+
+        pipe_ind = 0
+        if len(birds) > 0:
+            if len(pipes) > 1 and birds[0].x > pipes[0].x + pipes[0].PIPE_TOP.get_width():
+                pipe_ind += 1
+        else:
+            run = False
+            break
+        
+        for x, bird in enumerate(birds):
+            bird.move()
+            ge[x].fitness += 0.1
+            output = nets[x].activate((bird.y, abs(bird.y - pipes[pipe_ind].height), abs(bird.y - pipes[pipe_ind].bottom)))
+
+            if output[0] > 0.5: 
+                bird.jump()
+
         add_pipe = False
         rem = []
         for pipe in pipes:
-            if pipe.collide(bird):
-                pass #꺼버려라
+            for x, bird in enumerate(birds):
+                if pipe.collide(bird):
+                    ge[x].fitness -= 1
+                    birds.pop(x)
+                    nets.pop(x)
+                    ge.pop(x)
+
+                if not pipe.passed and pipe.x < bird.x:
+                    pipe.passed = True
+                    add_pipe = True
+                
             if pipe.x + pipe.PIPE_TOP.get_width() < 0:
-                rem.append(pipe)
-            if not pipe.passed and pipe.x < bird.x:
-                pipe.passed = True
-                add_pipe = True
+                    rem.append(pipe)
+
             pipe.move()
         if add_pipe:
             score += 1
+            for g in ge:
+                g.fitness += 5
             pipes.append(Pipe(700))
+
         for r in rem:
             pipes.remove(r)
-        if bird.y + bird.img.get_height() > 730:
-            pass
+        for x, bird in enumerate(birds):
+            if bird.y + bird.img.get_height() > 730 or bird.y < 0:
+                birds.pop(x)
+                nets.pop(x)
+                ge.pop(x)
             
             
             
         # bird.move()
         base.move()
-        draw_window(win, bird, pipes, base, score)
-    pygame.quit()
-    quit()
+        draw_window(win, birds, pipes, base, score)
+    
 
-main()
-
-def run():
+def run(config_path):
     config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction, 
                         neat.DefaultSpeciesSet, neat.DefaultStagnation, config_path)
 
