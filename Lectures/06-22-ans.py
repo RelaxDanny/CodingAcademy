c9630242bf011605a13e1e41d774d3d13adf61ce
Youngho Kim <44015667+rxYoungho@users.noreply.github.com>
2020-07-13 20:32:24 +0900

    Update 06-22-ans.py

--- Lectures/06-22-ans.py
+++ Lectures/06-22-ans.py
@@ -3,15 +3,12 @@ def solution():
     arr.sort(reverse=True)
     for i in range(N):
         arr[i] = arr[i] * (i + 1)
- 
     return max(arr)
- 
- 
+    
 N = int(input())
 arr = []
 for _ in range(N):
     arr.append(int(input()))
- 
 print(solution())
 
 # 20
@@ -25,3 +22,5 @@ print(solution())
 # 120, 200, 240, 200, 100
 
 # 240
+
+
