96a1400263e1b36ebb220d52bb515f773fd03579
Youngho Kim <44015667+rxYoungho@users.noreply.github.com>
2020-07-13 20:32:38 +0900

    Create 체육관.py

---
+++ Algorithm/체육관.py
@@ -0,0 +1,39 @@
+def solution(n, lost, reserve):
+    answer = 0
+    tmp = [i+1 for i in range(n)]
+    tmp_zero = [1 for _ in range(n)]
+    for i in range(len(lost)):
+        for j in range(len(tmp)):
+            if lost[i] == tmp[j]:
+                tmp_zero[tmp.index(lost[i])] -= 1
+
+    for i in range(len(reserve)):
+        for j in range(len(tmp)):
+            if reserve[i] == tmp[j]:
+                tmp_zero[tmp.index(reserve[i])] += 1
+
+    answer = tmp_zero
+    for i in range(len(answer)):
+        if answer[i] == 2 and answer[i] == 2 :
+            if i >= 1:
+                if answer[i-1] == 0 and answer[i] == 2:
+                    # print(answer)
+                    answer[i] -= 1
+                    answer[i-1] += 1
+            elif i < len(answer)-1:
+                if answer[i+1] == 0:
+                    answer[i] -= 1
+                    answer[i+1] += 1
+                
+            
+            # if i >= 1:
+            
+    print(answer)
+    result = 0
+    for i in range(len(answer)):
+        if answer[i] != 0:
+            result += 1
+    return result
+
+
+print(solution(7, [1,3,5], [1,2,4,6])) 
