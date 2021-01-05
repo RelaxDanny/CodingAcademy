900a7312482b5f1ea7bf503abcbb1e76ba25389b
Youngho Kim <44015667+rxYoungho@users.noreply.github.com>
2021-01-05 21:04:12 +0900

    ,

--- JAVA_Tutorial/Lecture_1/src/lecture_1/Number.java
+++ JAVA_Tutorial/Lecture_1/src/lecture_1/Number.java
@@ -3,8 +3,30 @@ package lecture_1;
 public class Number {
 
 	public static void main(String[] args) {
-		System.out.println(1+2);
-
+		System.out.println(1+5);
+		System.out.println("Quote: \"I am a boy\"");
+		System.out.println("¿µÈ£"+"±è");
+		
+		int a;
+		a = 1;
+		System.out.println(a+1);
+		
+		String first = "coding";
+		System.out.println(first + " academy");
+		//
+		/* 
+		
+		long -> 8 byte  -9,223,372,036,854,775,808 ~ 9,223,372,036,854,775,807
+		byte -> 1 byte  -128 ~ 127
+		short -> 2 byte -32.768 ~ 32,767
+		int -> 4 byte -2,147,483,648 ~ 2,147,483,647
+		*/
 	}
-
+	
+	public static int number(int a, int b) {
+		int output;
+		output = a + b;
+		return output;
+	}
+	
 }
---
+++ JAVA_Tutorial/Lecture_1/src/lecture_2/tutorial_2.java
@@ -0,0 +1,9 @@
+package lecture_2;
+
+public class tutorial_2 {
+	
+	public static void main(String[] args) {
+		//DEMO:
+	}
+}
+
